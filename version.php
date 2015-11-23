00cfcf55f32b45cfa1d14de9792974fa1ecc8c09
Juan Leyva <juanleyvadelgado@gmail.com>
2015-11-23 21:23:54 +0100

    Bump version number for releasing new version

--- version.php
+++ version.php
@@ -22,8 +22,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$plugin->version  = 2015010125;
+$plugin->version  = 2015010126;
 $plugin->requires = 2012120300;
 $plugin->component = 'local_aspiredu';
-$plugin->release = '1.9';
+$plugin->release = '2.0';
 $plugin->maturity = MATURITY_STABLE;
