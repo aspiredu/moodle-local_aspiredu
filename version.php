259a77c06dffce48651585e155c004ae95c24ab9
Juan Leyva <juanleyvadelgado@gmail.com>
2017-02-14 20:33:20 +0100

    ump version number for releasing version 3.1.1

--- version.php
+++ version.php
@@ -22,8 +22,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$plugin->version  = 2016021901;
+$plugin->version  = 2016021902;
 $plugin->requires = 2015111600; // 3.0
 $plugin->component = 'local_aspiredu';
-$plugin->release = '3.1';
+$plugin->release = '3.1.1';
 $plugin->maturity = MATURITY_STABLE;
