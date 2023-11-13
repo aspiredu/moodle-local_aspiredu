9abc86a69a398f87ec52484c45c4f900560ad415
Tim Schilling <tim@aspiredu.com>
2023-11-13 13:46:26 -0600

    Version 5.0
    Drops support for Moodle <=3.11
    Adds support for Moodle 4+

--- version.php
+++ version.php
@@ -27,8 +27,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2023051709;
+$plugin->version = 2023111300;
 $plugin->requires = 2022041900.00;
 $plugin->component = 'local_aspiredu';
-$plugin->release = '4.1.0';
+$plugin->release = '5.0.0';
 $plugin->maturity = MATURITY_STABLE;
