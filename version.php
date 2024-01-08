5658505ae2a85c3e5ed10e1798759c606cfa3bbe
Tim Schilling <tim@aspiredu.com>
2024-01-08 11:21:58 -0600

    Remove redefinitions of core web services.
    These web services were originally added because the core web services
    didn't originally include these. Over time these were ported to the core
    moodle project rendering our definitions unnecessary. At this point we
    were just calling the core functions with our local names. This removes
    that layer and uses the core logic entirely.
    
    This was a required change for the OpenLMS code review.

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2023111300;
+$plugin->version = 2023111301;
 $plugin->requires = 2022041900.00;
 $plugin->component = 'local_aspiredu';
 $plugin->release = '5.0.0';
