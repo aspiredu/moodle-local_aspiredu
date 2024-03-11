167764643f8515d62a2103812fd29567c6163be7
Tim Schilling <tim@aspiredu.com>
2024-03-11 13:11:08 -0500

    Version 5.2.1
    - Adds web service for fetching the site administrators.

--- version.php
+++ version.php
@@ -27,8 +27,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2024030600;
+$plugin->version = 2024031100;
 $plugin->requires = 2022041900.00;
 $plugin->component = 'local_aspiredu';
-$plugin->release = '5.2.0';
+$plugin->release = '5.2.1';
 $plugin->maturity = MATURITY_STABLE;
