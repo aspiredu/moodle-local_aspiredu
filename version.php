d4f864acb78e2142a4f6177089c5f4b7d8bfb9a0
Tim Schilling <tim@aspiredu.com>
2024-05-07 09:53:10 -0500

    Version 5.2.3

--- version.php
+++ version.php
@@ -28,8 +28,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2024042400;
+$plugin->version = 2024050700;
 $plugin->requires = 2022041900.00;
 $plugin->component = 'local_aspiredu';
-$plugin->release = '5.2.2';
+$plugin->release = '5.2.3';
 $plugin->maturity = MATURITY_STABLE;
